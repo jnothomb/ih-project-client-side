099135f0fcf2f257fe2335ad4d01f10ea206ec74
Tiago Ornelas <titasornelas@gmail.com>
2017-12-13 18:23:22 +0100

    forms, route guards and overall functioning

--- src/app/pages/page-profile-individual/page-profile-individual.component.ts
+++ src/app/pages/page-profile-individual/page-profile-individual.component.ts
@@ -14,20 +14,25 @@ import { User } from '../../models/user';
 })
 export class PageProfileIndividualComponent implements OnInit {
   user;
-  id;
   error;
   feedbackEnabled = false;
 
   constructor(
     private authService: AuthService,
     private userService: UserService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     // this.user = this.authService.getUser();
     this.authService.me()
-      .then((user) => {
-        this.user = user;
+      .then((user: any) => {
+        this.userService.getUserProfile(user._id)
+          .subscribe((data) => {
+            console.log(data);
+            this.user = data;
+          });
       });
   }
 
@@ -36,10 +41,13 @@ export class PageProfileIndividualComponent implements OnInit {
     if (form.invalid) {
       return;
     }
-    this.userService.editProfile(this.user._id).subscribe(
-      () => console.log('change succesful'),
-      (err) => this.error = err // @toio how to capture the bqackend error message (is somehere inside the "err" vadiable)
-    );
+    this.userService.editProfile(this.user).subscribe(
+      (data) => {
+        this.user = data;
+        console.log(data);
+      },
+      (err) => this.error = err,
+      () => this.router.navigate(['/restaurants']));
   }
 
 }
